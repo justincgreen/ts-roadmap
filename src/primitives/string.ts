d87d8bdbabe7af315a45ccb3bdbe5b7e678680f7
Justin Green <justingreendev@gmail.com>
2024-10-02 15:12:50 -0500

    string type

---
+++ src/primitives/string.ts
@@ -0,0 +1,7 @@
+/*
+  string is a primitive data type in TypeScript that represents textual data. It is a set of elements of the 16-bit Unicode character set.
+*/
+
+let person: string = 'Bob Belcher';
+
+console.log(person);
