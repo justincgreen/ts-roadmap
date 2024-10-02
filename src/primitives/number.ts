82a75e2966608f821037f737d69b8fbc5566874f
Justin Green <justingreendev@gmail.com>
2024-10-02 15:11:18 -0500

    number type

---
+++ src/primitives/number.ts
@@ -0,0 +1,8 @@
+/*
+  number is a primitive data type in TypeScript that represents numeric values. It includes both integer and floating-point values.
+*/
+
+let intValue: number = 11;
+let floatValue: number = 11.1;
+
+console.log(intValue, floatValue);
