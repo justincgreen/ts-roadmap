2a0d3376a39d4a72fc589149320db9b1738ec505
Justin Green <justingreendev@gmail.com>
2024-10-02 15:09:22 -0500

    boolean

---
+++ src/primitives/boolean.ts
@@ -0,0 +1,7 @@
+/*
+  boolean is a primitive data type in TypeScript that represents a boolean value i.e. either true or false.
+*/
+let isTrue: boolean = true;
+let isFalse: boolean = false;
+
+console.log(isTrue, isFalse);
