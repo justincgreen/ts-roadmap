be55ebc07869c9892be736637717fca2659972e7
Justin Green <justingreendev@gmail.com>
2024-10-03 15:48:57 -0500

    generics

--- src/generics/types.ts
+++ src/generics/types.ts
@@ -1,3 +1,7 @@
+/*
+  Generic types in TypeScript allow you to write objects, functions and classes that work with multiple data types, instead of being limited to a single data type. A generic type is defined using angle brackets <T> and can be used as a placeholder for a specific data type. The actual data type is specified when the function or class is used.
+*/
+
 function identity<T>(arg: T): T {
   return arg;
 }
