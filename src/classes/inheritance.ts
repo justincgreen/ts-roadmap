3887101d4f6d9f88b1f41c4a0c690116e34a1826
Justin Green <justingreendev@gmail.com>
2024-10-05 11:11:22 -0500

    inheritance

---
+++ src/classes/inheritance.ts
@@ -0,0 +1,25 @@
+class Animal {
+  makeSound(): void {
+    console.log('Making animal sound')
+  }
+}
+
+class Dog extends Animal {
+  makeSound(): void {
+    console.log('Bark');
+  }
+}
+
+class Cat extends Animal {
+  makeSound(): void {
+    console.log('Meow');
+  }
+}
+
+let animal: Animal;
+
+animal = new Dog();
+animal.makeSound();
+
+animal = new Cat();
+animal.makeSound();
